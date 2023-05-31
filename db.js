340b169577fa9dae04ae766ef95efbd144d88ec0
SUMANTH BOTLAGUNTA <86908741+sumanth-botlagunta@users.noreply.github.com>
2023-05-31 17:08:54 +0530

    Update mongoose plocy db.js

--- db.js
+++ db.js
@@ -1,2 +1,4 @@
 var mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb+srv://sumanth:12345@sumanth.w8xsd.mongodb.net/medimax?retryWrites=true&w=majority')
